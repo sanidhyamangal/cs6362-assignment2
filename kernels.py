4317a02da821dd470f645a1e1c0f9529ea725901
Sanidhya <mangalsanidhya19@gmail.com>
2021-09-08 01:10:54 -0500

    feat: implemented squared exponential kernel
    added: form_posterior_mean method in SQE kernel

--- kernels.py
+++ kernels.py
@@ -39,13 +39,43 @@ class Kernel:
 class SquaredExponentialKernel(Kernel):
     # --- Constructor: length scale should be a d-dimensional vector (one scaling per dimension), and noise variancee is a number
     def __init__(self,X,y,length_scale,noise_variance):
-        pass
+        # initiate a base kernel for computing the cholesky decomp and kernel matrix
+        super(SquaredExponentialKernel, self).__init__(X,y)
+
+        # init hyper params
+        self.length_scale, self.noise_variance = length_scale, noise_variance
+
     #
 
     # --- compute squared exponential kernel between two data matrices: X_1 and X_2
     #   (1) is_train_kernel is a boolean flag indicating whether we are computing the kernel on _just_ the training data,
     #       in which case we add the noise variance to the diagonal
     def kernel(self,X_1,X_2,is_train_kernel):
-        pass
+        # To compute the square kernel we will use vector form and compute the eucledian distance using following formulae
+        # (a-b)^2 = a^2 + b^2 - 2ab
+        eculedian_distance = np.sum(X_1**2,axis=1).reshape(-1,1) + np.sum(X_2**2,axis=1) - 2*X_1@X_2.T
+
+        # compute the kernel function
+        _kernel = (np.exp(-0.5 * (1/self.length_scale)*eculedian_distance))
+
+        # return with noise term if in training mode
+        if is_train_kernel:
+            # compute noise term sigma^2I
+            noise_term = self.noise_variance**2 * (np.eye(N=_kernel.shape[0],M=_kernel.shape[1]))
+
+            _kernel += noise_term
+
+            return _kernel
+        
+        return _kernel
     #
+
+    def form_posterior_mean(self, X_star):
+        # compute K_star
+        K_star = self.kernel(X_star,self.X)
+        
+        # compute mean
+        _mean = K_star@np.linalg.inv(self.K)*self.y
+
+
 #
